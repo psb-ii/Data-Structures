1e8411dad48f809f9bcc98122091e1bcfc00010f
Prakhar Bhadouria <prakhar.bhadouria0108@gmail.com>
2025-12-15 21:30:35 +0530

    O(n) sort in single raversal without using extra space

---
+++ sort012.cpp
@@ -0,0 +1,23 @@
+#include<vector>
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int low = 0, mid = 0;
+        int high = nums.size() - 1;
+        while(mid <= high){
+            if(nums[mid] == 0){
+                swap(nums[mid],nums[low]);
+                mid++;
+                low++;
+            }
+            else{
+                if(nums[mid] == 1)
+                    mid++;
+                else{
+                    swap(nums[high],nums[mid]);
+                    high--;
+                }
+            }
+        }
+    }
+};
