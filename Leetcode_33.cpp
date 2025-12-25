4d9075a62a54340eb28450306e3689eb0c34e1c5
Prakhar Bhadouria <prakhar.bhadouria0108@gmail.com>
2025-12-25 23:21:47 +0530

    O(logn) O(1)

---
+++ Leetcode_33.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low = 0;
+        int high = nums.size() - 1;
+        int ind = -1;
+        while(low <= high){
+            int mid = low + ((high - low) >> 1);
+            if(nums[mid] == target)
+                return mid;
+            else
+            if(nums[low] <= nums[mid]){
+                if(nums[low] <= target && nums[mid] > target)
+                    high = mid - 1;
+                else
+                    low = mid + 1;
+            }
+            else{
+                if(target > nums[mid] && target <= nums[high])
+                    low = mid + 1;
+                else
+                    high = mid - 1; 
+            }
+        }
+        return ind;
+    }
+};
