57e1fb65dfafb5fbe0d90e8c857e5f708330fe39
Prakhar Bhadouria <prakhar.bhadouria0108@gmail.com>
2026-05-21 18:00:53 +0530

    O(E) O(E)

---
+++ Leetcode_1319.cpp
@@ -0,0 +1,46 @@
+class DisjointSet{
+    vector<int>rank;
+    vector<int>parent;
+public:
+    DisjointSet(int n){
+        rank.resize(n+1,0);
+        parent.resize(n+1);
+        for(int i=0;i<n+1;i=i+1){
+            parent[i]=i;
+        }
+    }
+    int findParent(int node){
+        if(parent[node] == node)
+            return node;
+        return parent[node] = findParent(parent[node]);
+    }
+    void unionByRank(int u,int v){
+        int ulpar_u = findParent(u);
+        int ulpar_v = findParent(v);
+        if(ulpar_u == ulpar_v)
+            return;
+        if(rank[ulpar_u] < rank[ulpar_v]){
+            parent[ulpar_u] = ulpar_v;
+        }
+        else
+        if(rank[ulpar_v] < rank[ulpar_u]){
+            parent[ulpar_v] = ulpar_u;
+        }
+        else{
+            parent[ulpar_u] = ulpar_v;
+            rank[ulpar_v] = rank[ulpar_u];
+        }
+    }
+    void unionBySize(int u,int v){
+        int ulpar_u = findParent(u);
+        int ulpar_v = findParent(v);
+        if(rank[ulpar_u] < rank[ulpar_v]){
+            rank[ulpar_v] = rank[ulpar_v] + rank[ulpar_u];
+            parent[ulpar_u] = ulpar_v;
+        }
+        else{
+            rank[ulpar_u] = rank[ulpar_u] + rank[ulpar_v];
+            parent[ulpar_v] = ulpar_u;
+        }
+    }
+};
